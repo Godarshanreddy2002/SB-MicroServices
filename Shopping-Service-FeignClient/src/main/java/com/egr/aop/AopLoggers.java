859bf57dc985c31484337e1babac4e194ab4a553
Godarshan2002 <godarshanreddyenamala@gmail.com>
2025-03-28 18:39:26 +0530

    spring boot AOP

---
+++ Shopping-Service-FeignClient/src/main/java/com/egr/aop/AopLoggers.java
@@ -0,0 +1,48 @@
+package com.egr.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+
+
+ 
+@Aspect
+@Component
+@Slf4j
+public class AopLoggers {
+ 
+    @Before("execution(* com.tekworks.rental.service.*.*(..))")
+    public void loggerBeforeService(JoinPoint joinPoint) {
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+ 
+        log.info("Executing Service method: " + methodName + "() in class: " + className);
+    }
+    @Before("execution(* com.tekworks.rental.controller.*.*(..))")
+    public void loggerBeforeController(JoinPoint joinPoint) {
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+ 
+        log.info("Executing Controller method: " + methodName + "() in class: " + className);
+    }
+    @After("execution(* com.tekworks.rental.service.*.*(..))")
+    public void loggerAfterService(JoinPoint joinPoint) {
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+ 
+        log.info("Execution Completed Service method: " + methodName + "() in class: " + className);
+    }
+    @After("execution(* com.tekworks.rental.controller.*.*(..))")
+    public void loggerAfterController(JoinPoint joinPoint) {
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+ 
+        log.info("Execution Completed  Controller method: " + methodName + "() in class: " + className);
+    }
+}
